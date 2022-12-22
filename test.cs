44974f983d0ca03194d3cc4b19019130e0147348
Something-Inconspicuous <webhead220@gmail.com>
2022-12-22 16:08:48 -0500

    Added an asset and code for assets
    Also added code to instantiate one of the assets and that should make console commands, but will test later

--- test.cs
+++ test.cs
@@ -3,14 +3,35 @@ using RaftModLoader;
 using HMLLibrary;
 
 public class Testytest : Mod{
-    public void Start()
+
+    AssetBundle asset;
+
+    public IEnumerator Start()
     {
+
+        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(GetEmbeddedFileBytes("testingBundle.assets"));
+        yield return request;
+        asset = request.assetBundle;
+
+        GameObject testingTable = asset.LoadAsset<GameObject>("table_and_chair");
+        Instantiate(testingTable);
+
+
         Debug.Log("Mod Testytest has been loaded!");
         
     }
 
     public void OnModUnload()
     {
+        asset.Unload(true);
         Debug.Log("Mod Testytest has been unloaded!");
     }
+
+    [ConsoleCommand(name: "...", docs: "...")]
+    public static void dotdotdot(string[] args){
+        
+        //...
+        
+    }
+
 }
